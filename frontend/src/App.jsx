498d29ddaca3a172ec26b1c97835978627be0cc5
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-07-16 20:49:20 +0530

    initialized frontend and tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./globals.css";
+
+const App = () => {
+  return <div className="bg-primar">App</div>;
+};
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+// const defaultTheme = import("tailwindcss/defaultTheme");
+
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "primary-500": "#de283b",
+        "primary-600": "#ff6366",
+        "secondary-500": "#ffccc4",
+        "off-white": "#D0DFFF",
+        "dark-1": "#000000",
+        "dark-2": "#09090A",
+        "dark-3": "#101012",
+        "dark-4": "#1F1F22",
+        "light-1": "#FFFFFF",
+        "light-2": "#EFEFEF",
+        "light-3": "#ffccc4",
+        "light-4": "#5C5C7B",
+      },
+      screens: {
+        xs: "480px",
+      },
+      width: {
+        420: "420px",
+        465: "465px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};
